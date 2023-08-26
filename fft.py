10c6703291f240d59b6679693c0f450022564fa2
SatyrDiamond <satyrdiamond@gmail.com>
2023-08-26 07:02:07 -0400

    update

--- fft.py
+++ fft.py
@@ -10,6 +10,7 @@ import dataclasses
 from functions import song
 from functions import tracks
 from functions import placement_data
+from functions import note_data
 import plugin_input
 
 def note_str(k):
@@ -19,10 +20,7 @@ def note_str(k):
 
 
 def nearest_note(x):
-    # x = 440 * 2 ^ (k / 12)
-    # k = 12 * log2(x / 440)
-    k = round(12 * math.log2(x / 440))
-    return k
+    return note_data.freq_to_note(x)[0]
 
 
 def note_to_freq(k):
@@ -41,24 +39,15 @@ FIXED_CHUNKS_PER_SECOND = 100
 NUM_CRINGE_HARMONICS = 3
 FREQUENCY_SMOOTHING = 0
 TARGET_RMS_VOLUME = 0.3
-MINIMUM_NOTE_VOLUME = 0.03
-NUM_FREQUENCY_BANDS = 10
+MINIMUM_NOTE_VOLUME = 0.4
+NUM_FREQUENCY_BANDS = 5
 FREQUENCY_BAND_RANDOM_CHUNKING_THRESHOLD = 1000
 TEMPO = 110
 
 # Don't mess with these though.
-MAXIMUM_NOTE = 40
-MINIMUM_NOTE = -33
+MAXIMUM_NOTE = 70
+MINIMUM_NOTE = -70
 CUTOFF_FREQ = note_to_freq(MAXIMUM_NOTE + 1)
-
-PIANO = [
-    'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2', 'C3', 'C#3', 'D3', 'D#3',
-    'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4',
-    'G#4', 'A4', 'A#4', 'B4', 'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5',
-    'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6', 'C7', 'C#7', 'D7', 'D#7',
-    'E7', 'F7', 'F#7', 'G7', 'G#7', 'A7', 'A#7', 'B7'
-]
-
 
 def clamp(x):
     return min(max(x, -32768), 32767)
